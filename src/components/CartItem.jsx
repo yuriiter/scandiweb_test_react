11fbc1388f2945a891131ffa10cb9ce48057b816
Yurii Tereshchenko <yurater007@gmail.com>
2022-08-15 17:33:58 +0200

    feat: cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,56 @@
 import { Component } from 'react'
+import plusIcon from "../assets/img/plus.svg";
+import minusIcon from "../assets/img/minus.svg";
+import cartImage from "../assets/img/cart_navigation_placeholder.jpg";
 
 class CartItem extends Component {
   render () {
     return (
-      <div>
+        <div className="cart__item-wrapper px-20">
+          <div className="cart__item d-flex justify-content-between">
+            <div className="cart__item-detail__info">
+              <h2 className="cart__item-detail__info-title">Apollo</h2>
+              <h3 className="cart__item-detail__info-subtitle">Running Short</h3>
 
-      </div>
+              <div className="info__price">
+                <span className={"info__price-value"}>$50.00</span>
+              </div>
+
+              <div className="info__size">
+                <span>Size:</span>
+                <div className="info__size-items">
+                  <div className={"info__size-items--active"}><span>xs</span></div>
+                  <div><span>s</span></div>
+                  <div><span>m</span></div>
+                  <div><span>l</span></div>
+                </div>
+              </div>
+
+              <div className="info__color">
+                <span>Color:</span>
+                <div className="info__color-items">
+                  <div className={"info__color-items--active"} style={{backgroundColor: "#614145"}}></div>
+                  <div style={{backgroundColor: "#614145"}}></div>
+                  <div style={{backgroundColor: "#614145"}}></div>
+                  <div style={{backgroundColor: "#614145"}}></div>
+                </div>
+              </div>
+
+            </div>
+
+            <div className="cart__item-gallery d-flex">
+              <div className="cart__counter d-flex flex-direction-column justify-content-between">
+                <button><img src={plusIcon} alt="Increment counter" /></button>
+                <span className="cart__count">3</span>
+                <button><img src={minusIcon} alt="Decrement counter" /></button>
+              </div>
+              <div className="cart__image">
+                <img src={cartImage} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+
     )
   }
 }
--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -1,66 +1,41 @@
 import { Component } from 'react'
 import Navigation from '../components/Navigation.jsx'
 import {connect} from "react-redux";
+import CartItem from "../components/CartItem";
 
 class Cart extends Component {
   render () {
     return (
         <div>
           <Navigation />
-          <section className="product-detail">
+          <section className="cart">
             <div className="container">
-              <div className="row">
-
-                <div className="product-detail__gallery d-flex px-20">
-                  <div style={{paddingRight: "20px"}} className="gallery__photos d-flex">
-                    <img src={null} alt="" />
-                    <img src={null} alt="" />
-                    <img src={null} alt="" />
-                  </div>
-                  <img className="gallery__main" src={null} alt="" />
-                </div>
-
-                <div className="product-detail__info px-20">
-                  <h2 className="product-detail__info-title">Apollo</h2>
-                  <h3 className="product-detail__info-subtitle">Running Short</h3>
-                  <div className="info__size">
-                    <span>Size:</span>
-                    <div className="info__size-items">
-                      <div className={"info__size-items--active"}><span>xs</span></div>
-                      <div><span>s</span></div>
-                      <div><span>m</span></div>
-                      <div><span>l</span></div>
-                    </div>
-                  </div>
-                  <div className="info__color">
-                    <span>Color:</span>
-                    <div className="info__color-items">
-                      <div className={"info__color-items--active"} style={{backgroundColor: "#614145"}}></div>
-                      <div style={{backgroundColor: "#614145"}}></div>
-                      <div style={{backgroundColor: "#614145"}}></div>
-                      <div style={{backgroundColor: "#614145"}}></div>
-                    </div>
-                  </div>
-
-                  <div className="info__price">
-                    <span>Price:</span>
-                    <span className={"info__price-value"}>$50.00</span>
-                  </div>
-
-                  <button className="info__button">
-                    <span>ADD TO CART</span>
-                  </button>
-
-                  <p className="info__description">
-                    Find stunning women's cocktail dresses and party dresses.
-                    Stand out in lace and metallic cocktail
-                    dresses and party dresses from all your favorite brands.
-                  </p>
-                </div>
-
-                <div className="product-detail__spacer"></div>
+              <h2 className={"cart__title"}>cart</h2>
+              <div className="cart__items row splitter">
+                <CartItem />
+                <CartItem />
               </div>
+
+              <div className="cart__price-checkout d-flex">
+                <div className="cart__price-checkout--left">
+                  <p>Tax 21%:</p>
+                  <p>Quantity:</p>
+                  <p>Total:</p>
+                </div>
+
+                <div className="cart__price-checkout--right">
+                  <p>$42.00</p>
+                  <p>3</p>
+                  <p>$200.00</p>
+                </div>
+              </div>
+
+              <button className="info__button">
+                <span>ORDER</span>
+              </button>
+
             </div>
+
             <div className={"category__overlay" + (this.props.isCartOpen ? "" : " d-none")}></div>
           </section>
         </div>
